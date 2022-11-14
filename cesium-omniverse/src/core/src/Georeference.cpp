129a406a660b445f226432389ec63c37af5d8393
Sean Lilley <lilleyse@gmail.com>
2022-11-14 08:00:26 -0500

    Convert meters to centimeters

--- cesium-omniverse/src/core/src/Georeference.cpp
+++ cesium-omniverse/src/core/src/Georeference.cpp
@@ -2,6 +2,7 @@
 
 #include <CesiumGeometry/AxisTransforms.h>
 #include <CesiumGeospatial/Transforms.h>
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace Cesium {
 Georeference& Georeference::instance() {
@@ -10,8 +11,9 @@ Georeference& Georeference::instance() {
 }
 
 void Georeference::setOrigin(const glm::dvec3& origin) {
-    relToAbsWorld =
-        CesiumGeospatial::Transforms::eastNorthUpToFixedFrame(origin) * CesiumGeometry::AxisTransforms::Y_UP_TO_Z_UP;
+    const auto centimeterToMeter = glm::scale(glm::dmat4(1.0), glm::dvec3(0.01));
+    relToAbsWorld = CesiumGeospatial::Transforms::eastNorthUpToFixedFrame(origin) *
+                    CesiumGeometry::AxisTransforms::Y_UP_TO_Z_UP * centimeterToMeter;
     absToRelWorld = glm::inverse(relToAbsWorld);
     originChangeEvent.invoke(relToAbsWorld, absToRelWorld);
 }
