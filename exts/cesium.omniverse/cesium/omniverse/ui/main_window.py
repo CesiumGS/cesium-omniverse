35afff6d256d44f48d679ff3061a831f122a2d13
Adam N. Morris <adam@cesium.com>
2023-01-19 11:09:13 -0600

    Adjusted button style slightly and changed how we are getting icon path

--- exts/cesium.omniverse/cesium/omniverse/ui/main_window.py
+++ exts/cesium.omniverse/cesium/omniverse/ui/main_window.py
@@ -1,10 +1,9 @@
 from ..bindings import ICesiumOmniverseInterface
 import logging
+import omni.kit.app as app
 import omni.ui as ui
 from pathlib import Path
 from typing import Optional
-
-ICON_PATH = Path(__file__).parent.parent.parent.parent.absolute().joinpath(f"icons")
 
 
 class CesiumOmniverseMainWindow(ui.Window):
@@ -17,9 +16,12 @@ class CesiumOmniverseMainWindow(ui.Window):
 
     def __init__(self, cesium_omniverse_interface: ICesiumOmniverseInterface, **kwargs):
         super().__init__(CesiumOmniverseMainWindow.WINDOW_NAME, **kwargs)
+        manager = app.get_app().get_extension_manager()
+        ext_id = manager.get_extension_id_by_module("cesium.omniverse")
 
         self._cesium_omniverse_interface = cesium_omniverse_interface
         self._logger = logging.getLogger(__name__)
+        self._icon_path = Path(manager.get_extension_path(ext_id)).joinpath("icons")
 
         # Buttons aren't created until the build function is called.
         self._add_button: Optional[ui.Button] = None
@@ -47,25 +49,25 @@ class CesiumOmniverseMainWindow(ui.Window):
                     "alignment": ui.Alignment.CENTER,
                 },
                 "Button.Label": {
-                    "alignment": ui.Alignment.CENTER
+                    "alignment": ui.Alignment.CENTER_BOTTOM
                 }
             }
 
-            with ui.HStack(height=ui.Length(15, ui.UnitType.PERCENT)):
-                self._add_button = ui.Button("Add", image_url=f"{ICON_PATH}/FontAwesome/plus-solid.png",
+            with ui.HStack(height=ui.Length(80, ui.UnitType.PIXEL)):
+                self._add_button = ui.Button("Add", image_url=f"{self._icon_path}/FontAwesome/plus-solid.png",
                                              style=button_style, clicked_fn=self._add_button_clicked, enabled=False)
                 self._upload_button = ui.Button("Upload",
-                                                image_url=f"{ICON_PATH}/FontAwesome/cloud-upload-alt-solid.png",
+                                                image_url=f"{self._icon_path}/FontAwesome/cloud-upload-alt-solid.png",
                                                 style=button_style, clicked_fn=self._upload_button_clicked,
                                                 enabled=False)
-                self._token_button = ui.Button("Token", image_url=f"{ICON_PATH}/FontAwesome/key-solid.png",
+                self._token_button = ui.Button("Token", image_url=f"{self._icon_path}/FontAwesome/key-solid.png",
                                                style=button_style, clicked_fn=self._token_button_clicked)
-                self._learn_button = ui.Button("Learn", image_url=f"{ICON_PATH}/FontAwesome/book-reader-solid.png",
+                self._learn_button = ui.Button("Learn", image_url=f"{self._icon_path}/FontAwesome/book-reader-solid.png",
                                                style=button_style, clicked_fn=self._learn_button_clicked)
-                self._help_button = ui.Button("Help", image_url=f"{ICON_PATH}/FontAwesome/hands-helping-solid.png",
+                self._help_button = ui.Button("Help", image_url=f"{self._icon_path}/FontAwesome/hands-helping-solid.png",
                                               style=button_style, clicked_fn=self._help_button_clicked)
                 self._sign_out_button = ui.Button("Sign Out",
-                                                  image_url=f"{ICON_PATH}/FontAwesome/sign-out-alt-solid.png",
+                                                  image_url=f"{self._icon_path}/FontAwesome/sign-out-alt-solid.png",
                                                   style=button_style, clicked_fn=self._sign_out_button_clicked,
                                                   enabled=False)
             ui.Label("TODO: The rest of this window.")
