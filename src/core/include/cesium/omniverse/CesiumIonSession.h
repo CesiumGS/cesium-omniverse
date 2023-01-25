95491cd6f438bdf0e61489111620ca478a19b50d
Adam N. Morris <adam@cesium.com>
2023-01-25 13:34:02 -0600

    Stubbed out CesiumIonSession

---
+++ src/core/include/cesium/omniverse/CesiumIonSession.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include <CesiumAsync/AsyncSystem.h>
+#include <CesiumAsync/IAssetAccessor.h>
+#include <CesiumAsync/SharedFuture.h>
+#include <CesiumIonClient/Connection.h>
+
+#include <memory>
+#include <optional>
+#include <vector>
+
+namespace cesium::omniverse {
+
+class CesiumIonSession {
+  public:
+    CesiumIonSession(
+        CesiumAsync::AsyncSystem& asyncSystem,
+        const std::shared_ptr<CesiumAsync::IAssetAccessor>& pAssetAccessor);
+
+    const std::shared_ptr<CesiumAsync::IAssetAccessor>& getAssetAccessor() const {
+        return this->_pAssetAccessor;
+    }
+    const CesiumAsync::AsyncSystem& getAsyncSystem() const {
+        return this->_asyncSystem;
+    }
+    CesiumAsync::AsyncSystem& getAsyncSystem() {
+        return this->_asyncSystem;
+    }
+
+    bool isConnected() const {
+        return this->_connection.has_value();
+    }
+    bool isConnecting() const {
+        return this->_isConnecting;
+    }
+    bool isResuming() const {
+        return this->_isResuming;
+    }
+
+    bool isProfileLoaded() const {
+        return this->_profile.has_value();
+    }
+    bool isLoadingProfile() const {
+        return this->_isLoadingProfile;
+    }
+
+    bool isAssetListLoaded() const {
+        return this->_assets.has_value();
+    }
+    bool isLoadingAssetList() const {
+        return this->_isLoadingAssets;
+    }
+
+    bool isTokenListLoaded() const {
+        return this->_tokens.has_value();
+    }
+    bool isLoadingTokenList() const {
+        return this->_isLoadingTokens;
+    }
+
+    void connect();
+    void resume();
+    void disconnect();
+
+    void refreshProfile();
+    void refreshAssets();
+    void refreshTokens();
+
+    const std::optional<CesiumIonClient::Connection>& getConnection() const;
+    const CesiumIonClient::Profile& getProfile();
+    const CesiumIonClient::Assets& getAssets();
+    const std::vector<CesiumIonClient::Token>& getTokens();
+
+    const std::string& getAuthorizeUrl() const {
+        return this->_authorizeUrl;
+    }
+
+    bool refreshProfileIfNeeded();
+    bool refreshAssetsIfNeeded();
+    bool refreshTokensIfNeeded();
+
+    /**
+   * Finds the details of the specified token in the user's account.
+   *
+   * If this session is not connected, returns std::nullopt.
+   *
+   * Even if the list of tokens is already loaded, this method does a new query
+   * in order get the most up-to-date information about the token.
+   *
+   * @param token The token.
+   * @return The details of the token, or an error response if the token does
+   * not exist in the signed-in user account.
+     */
+    CesiumAsync::Future<CesiumIonClient::Response<CesiumIonClient::Token>> findToken(const std::string& token) const;
+
+    /**
+   * Gets the project default token.
+   *
+   * If the project default token exists in the signed-in user's account, full
+   * token details will be included. Otherwise, only the token value itself
+   * (i.e. the `token` property`) will be included, and it may or may not be
+   * valid. In the latter case, the `id` property will be an empty string.
+   *
+   * @return A future that resolves to the project default token.
+     */
+    CesiumAsync::SharedFuture<CesiumIonClient::Token> getProjectDefaultTokenDetails();
+
+    void invalidateProjectDefaultTokenDetails();
+
+  private:
+    CesiumAsync::AsyncSystem _asyncSystem;
+    std::shared_ptr<CesiumAsync::IAssetAccessor> _pAssetAccessor;
+
+    std::optional<CesiumIonClient::Connection> _connection;
+    std::optional<CesiumIonClient::Profile> _profile;
+    std::optional<CesiumIonClient::Assets> _assets;
+    std::optional<std::vector<CesiumIonClient::Token>> _tokens;
+
+    std::optional<CesiumAsync::SharedFuture<CesiumIonClient::Token>> _projectDefaultTokenDetailsFuture;
+
+    bool _isConnecting;
+    bool _isResuming;
+    bool _isLoadingProfile;
+    bool _isLoadingAssets;
+    bool _isLoadingTokens;
+
+    bool _loadProfileQueued;
+    bool _loadAssetsQueued;
+    bool _loadTokensQueued;
+
+    std::string _authorizeUrl;
+};
+} // namespace cesium::omniverse
---
+++ src/core/src/CesiumIonSession.cpp
@@ -0,0 +1,335 @@
+// Copyright 2020-2021 CesiumGS, Inc. and Contributors
+
+#include "cesium/omniverse/CesiumIonSession.h"
+
+using namespace CesiumAsync;
+using namespace CesiumIonClient;
+
+using namespace cesium::omniverse;
+
+CesiumIonSession::CesiumIonSession(
+    CesiumAsync::AsyncSystem& asyncSystem,
+    const std::shared_ptr<CesiumAsync::IAssetAccessor>& pAssetAccessor)
+    : _asyncSystem(asyncSystem),
+    _pAssetAccessor(pAssetAccessor),
+    _connection(std::nullopt),
+    _profile(std::nullopt),
+    _assets(std::nullopt),
+    _tokens(std::nullopt),
+    _isConnecting(false),
+    _isResuming(false),
+    _isLoadingProfile(false),
+    _isLoadingAssets(false),
+    _isLoadingTokens(false),
+    _loadProfileQueued(false),
+    _loadAssetsQueued(false),
+    _loadTokensQueued(false),
+    _authorizeUrl() {}
+
+void CesiumIonSession::connect() {
+    if (this->isConnecting() || this->isConnected() || this->isResuming()) {
+        return;
+    }
+
+    this->_isConnecting = true;
+
+    Connection::authorize(
+        this->_asyncSystem,
+        this->_pAssetAccessor,
+        "Cesium for Omniverse",
+        190,
+        "/cesium-for-Omniverse/oauth2/callback",
+        {"assets:list",
+         "assets:read",
+         "profile:read",
+         "tokens:read",
+         "tokens:write",
+         "geocode"},
+        [this](const std::string& url) {
+            this->_authorizeUrl = url;
+
+            // TODO: Launch web browser.
+        })
+        .thenInMainThread([this](CesiumIonClient::Connection&& connection) {
+            this->_isConnecting = false;
+            this->_connection = std::move(connection);
+
+            // How to get the access token.
+            // this->_connection.value().getAccessToken().c_str()
+
+            // TODO: Save the access token to settings and broadcast that the connection is updated.
+        })
+        .catchInMainThread([this]([[maybe_unused]] std::exception&& e) {
+            this->_isConnecting = false;
+            this->_connection = std::nullopt;
+
+            // TODO: Broadcast that the connection is updated.
+        });
+}
+
+void CesiumIonSession::resume() {
+    if (this->isConnecting() || this->isConnected() || this->isResuming()) {
+        return;
+    }
+
+    // TODO: Get userAccessToken from settings storage.
+    std::string userAccessToken = "";
+    if (userAccessToken.empty()) {
+        // No existing session to resume.
+        return;
+    }
+
+    this->_isResuming = true;
+
+    this->_connection = Connection(
+        this->_asyncSystem,
+        this->_pAssetAccessor,
+        userAccessToken);
+
+    // Verify that the connection actually works.
+    this->_connection.value()
+        .me()
+        .thenInMainThread([this](Response<Profile>&& response) {
+            if (!response.value.has_value()) {
+                this->_connection.reset();
+            }
+            this->_isResuming = false;
+            // TODO: Broadcast connection updated.
+        })
+        .catchInMainThread([this]([[maybe_unused]] std::exception&& e) {
+            this->_isResuming = false;
+            this->_connection.reset();
+        });
+}
+
+void CesiumIonSession::disconnect() {
+    this->_connection.reset();
+    this->_profile.reset();
+    this->_assets.reset();
+    this->_tokens.reset();
+
+    // TODO: Clear out the user access token from settings storage.
+
+    // TODO: Broadcast connection updated.
+    // TODO: Broadcast profile updated.
+    // TODO: Broadcast assets updated.
+    // TODO: Broadcast tokens updated.
+}
+
+void CesiumIonSession::refreshProfile() {
+    if (!this->_connection || this->_isLoadingProfile) {
+        this->_loadProfileQueued = true;
+        return;
+    }
+
+    this->_isLoadingProfile = true;
+    this->_loadProfileQueued = false;
+
+    this->_connection->me()
+        .thenInMainThread([this](Response<Profile>&& profile) {
+            this->_isLoadingProfile = false;
+            this->_profile = std::move(profile.value);
+            // TODO: Broadcast profile updated.
+            this->refreshProfileIfNeeded();
+        })
+        .catchInMainThread([this]([[maybe_unused]] std::exception&& e) {
+            this->_isLoadingProfile = false;
+            this->_profile = std::nullopt;
+            // TODO: Broadcast profile updated.
+            this->refreshProfileIfNeeded();
+        });
+}
+
+void CesiumIonSession::refreshAssets() {
+    if (!this->_connection || this->_isLoadingAssets) {
+        return;
+    }
+
+    this->_isLoadingAssets = true;
+    this->_loadAssetsQueued = false;
+
+    this->_connection->assets()
+        .thenInMainThread([this](Response<Assets>&& assets) {
+            this->_isLoadingAssets = false;
+            this->_assets = std::move(assets.value);
+            // TODO: Broadcast assets updated.
+            this->refreshAssetsIfNeeded();
+        })
+        .catchInMainThread([this]([[maybe_unused]] std::exception&& e) {
+            this->_isLoadingAssets = false;
+            this->_assets = std::nullopt;
+            // TODO: Broadcast assets updated.
+            this->refreshAssetsIfNeeded();
+        });
+}
+
+void CesiumIonSession::refreshTokens() {
+    if (!this->_connection || this->_isLoadingTokens) {
+        return;
+    }
+
+    this->_isLoadingTokens = true;
+    this->_loadTokensQueued = false;
+
+    this->_connection->tokens()
+        .thenInMainThread([this](Response<TokenList>&& tokens) {
+            this->_isLoadingTokens = false;
+            this->_tokens = tokens.value
+                                ? std::make_optional(std::move(tokens.value->items))
+                                : std::nullopt;
+            // TODO: Broadcast tokens updated.
+            this->refreshTokensIfNeeded();
+        })
+        .catchInMainThread([this]([[maybe_unused]] std::exception&& e) {
+            this->_isLoadingTokens = false;
+            this->_tokens = std::nullopt;
+            // TODO: Broadcast tokens updated.
+            this->refreshTokensIfNeeded();
+        });
+}
+
+const std::optional<CesiumIonClient::Connection>&
+CesiumIonSession::getConnection() const {
+    return this->_connection;
+}
+
+const CesiumIonClient::Profile& CesiumIonSession::getProfile() {
+    static const CesiumIonClient::Profile empty{};
+    if (this->_profile) {
+        return *this->_profile;
+    } else {
+        this->refreshProfile();
+        return empty;
+    }
+}
+
+const CesiumIonClient::Assets& CesiumIonSession::getAssets() {
+    static const CesiumIonClient::Assets empty;
+    if (this->_assets) {
+        return *this->_assets;
+    } else {
+        this->refreshAssets();
+        return empty;
+    }
+}
+
+const std::vector<CesiumIonClient::Token>& CesiumIonSession::getTokens() {
+    static const std::vector<CesiumIonClient::Token> empty;
+    if (this->_tokens) {
+        return *this->_tokens;
+    } else {
+        this->refreshTokens();
+        return empty;
+    }
+}
+
+bool CesiumIonSession::refreshProfileIfNeeded() {
+    if (this->_loadProfileQueued || !this->_profile.has_value()) {
+        this->refreshProfile();
+    }
+    return this->isProfileLoaded();
+}
+
+bool CesiumIonSession::refreshAssetsIfNeeded() {
+    if (this->_loadAssetsQueued || !this->_assets.has_value()) {
+        this->refreshAssets();
+    }
+    return this->isAssetListLoaded();
+}
+
+bool CesiumIonSession::refreshTokensIfNeeded() {
+    if (this->_loadTokensQueued || !this->_tokens.has_value()) {
+        this->refreshTokens();
+    }
+    return this->isTokenListLoaded();
+}
+
+Future<Response<Token>>
+CesiumIonSession::findToken(const std::string& token) const {
+    if (!this->_connection) {
+        return this->getAsyncSystem().createResolvedFuture(
+            Response<Token>(0, "NOTCONNECTED", "Not connected to Cesium ion."));
+    }
+
+    std::optional<std::string> maybeTokenID =
+        Connection::getIdFromToken(token);
+
+    if (!maybeTokenID) {
+        return this->getAsyncSystem().createResolvedFuture(
+            Response<Token>(0, "INVALIDTOKEN", "The token is not valid."));
+    }
+
+    return this->_connection->token(*maybeTokenID);
+}
+
+namespace {
+
+Token tokenFromSettings() {
+    Token result;
+    // TODO: Get the token from settings.
+//    result.token = TCHAR_TO_UTF8(
+//        *GetDefault<UCesiumRuntimeSettings>()->DefaultIonAccessToken);
+    return result;
+}
+
+Future<Token> getTokenFuture(const CesiumIonSession& session) {
+    // TODO: Get the default ion access token from settings.
+    std::string defaultIonAccessToken = "";
+
+    if (!defaultIonAccessToken.empty()) {
+        return session.getConnection()
+            ->token(defaultIonAccessToken)
+            .thenImmediately([](Response<Token>&& tokenResponse) {
+                if (tokenResponse.value) {
+                    return *tokenResponse.value;
+                } else {
+                    return tokenFromSettings();
+                }
+            });
+    } else if (!defaultIonAccessToken.empty()) {
+        return session
+            .findToken(defaultIonAccessToken)
+            .thenImmediately([](Response<Token>&& response) {
+                if (response.value) {
+                    return *response.value;
+                } else {
+                    return tokenFromSettings();
+                }
+            });
+    } else {
+        return session.getAsyncSystem().createResolvedFuture(tokenFromSettings());
+    }
+}
+
+} // namespace
+
+SharedFuture<Token> CesiumIonSession::getProjectDefaultTokenDetails() {
+    if (this->_projectDefaultTokenDetailsFuture) {
+        // TODO: Get the default ion access token from settings.
+        std::string defaultIonAccessToken = "";
+
+        // If the future is resolved but its token doesn't match the designated
+        // default token, do the request again because the user probably specified a
+        // new token.
+        if (this->_projectDefaultTokenDetailsFuture->isReady() &&
+            this->_projectDefaultTokenDetailsFuture->wait().token !=
+                defaultIonAccessToken) {
+            this->_projectDefaultTokenDetailsFuture.reset();
+        } else {
+            return *this->_projectDefaultTokenDetailsFuture;
+        }
+    }
+
+    if (!this->isConnected()) {
+        return this->getAsyncSystem()
+            .createResolvedFuture(tokenFromSettings())
+            .share();
+    }
+
+    this->_projectDefaultTokenDetailsFuture = getTokenFuture(*this).share();
+    return *this->_projectDefaultTokenDetailsFuture;
+}
+
+void CesiumIonSession::invalidateProjectDefaultTokenDetails() {
+    this->_projectDefaultTokenDetailsFuture.reset();
+}
