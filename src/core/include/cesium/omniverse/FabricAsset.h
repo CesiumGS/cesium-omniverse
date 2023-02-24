0f67cb56ac1e6fbfcd81b35b1521309cfbf31ee7
Sean Lilley <lilleyse@gmail.com>
2023-02-23 20:13:25 -0500

    Update FabricAsset for Linux

--- src/core/include/cesium/omniverse/FabricAsset.h
+++ src/core/include/cesium/omniverse/FabricAsset.h
@@ -11,8 +11,8 @@ namespace cesium::omniverse {
 
 // We don't actually know the struct layout for assets since they are internal to Fabric
 // but we know that the first field is an asset path and the second field is a resolved path
-// corresponding to SdfAssetPath::GetAssetPath and SdfAssetPath::GetResolvedPath and that
-// the remaining 48 bytes are zero (at least from what we've seen so far)
+// corresponding to SdfAssetPath::GetAssetPath and SdfAssetPath::GetResolvedPath. On Windows
+// the object is padded out to 64 bytes (zeroed). On Linux there is no padding.
 
 class FabricAsset {
   public:
@@ -23,10 +23,17 @@ class FabricAsset {
     const char* getAssetPath() const;
     const char* getResolvedPath() const;
 
-  private:
     pxr::TfToken _assetPath;
     pxr::TfToken _resolvedPath;
+#ifdef CESIUM_OMNI_WINDOWS
     std::array<std::byte, 48> _padding{};
+#endif
 };
+
+#ifdef CESIUM_OMNI_WINDOWS
+static_assert(sizeof(FabricAsset) == 64);
+#else
+static_assert(sizeof(FabricAsset) == 16);
+#endif
 
 } // namespace cesium::omniverse
