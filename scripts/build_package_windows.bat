7b889333c668022318db510a5ff1508efdd389a4
Sean Lilley <lilleyse@gmail.com>
2023-03-01 12:40:58 -0500

    Fix Windows batch script

--- scripts/build_package_windows.bat
+++ scripts/build_package_windows.bat
@@ -1,4 +1,7 @@
-cmd /c cmake -B build-package -D CMAKE_BUILD_TYPE=Release CESIUM_OMNI_ENABLE_TESTS=OFF -D CESIUM_OMNI_ENABLE_DOCUMENTATION=OFF -D CESIUM_OMNI_ENABLE_SANITIZERS=OFF -D CESIUM_OMNI_ENABLE_LINTERS=OFF
-cmd /c cmake --build build --config Release
-cmd /c cmake --build build --target install --config Release
-cmd /c cmake --build build --target package --config Release
+@echo off
+
+rmdir /s /q build-package
+cmd /c cmake -B build-package -D CMAKE_CONFIGURATION_TYPES=Release -D CESIUM_OMNI_ENABLE_TESTS=OFF -D CESIUM_OMNI_ENABLE_DOCUMENTATION=OFF -D CESIUM_OMNI_ENABLE_SANITIZERS=OFF -D CESIUM_OMNI_ENABLE_LINTERS=OFF
+cmd /c cmake --build build-package --config Release
+cmd /c cmake --build build-package --target install --config Release
+cmd /c cmake --build build-package --target package --config Release
