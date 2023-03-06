774addd0a99cbb52b43a54f33b219bd69b4663b2
Adam N. Morris <adam@cesium.com>
2023-03-06 14:11:27 -0600

    Fixed a bad formatting change

--- exts/cesium.omniverse/cesium/omniverse/__init__.py
+++ exts/cesium.omniverse/cesium/omniverse/__init__.py
@@ -3,7 +3,7 @@ from .extension import *  # noqa: F401 F403 F405
 from .utils import *  # noqa: F401 F403 F405
 from pxr import Plug
 
-pluginsRoot = extension.os.path.join(extension.os.path.dirname(__file__), "../../plugins")
+pluginsRoot = os.path.join(os.path.dirname(__file__), "../../plugins")
 inMemoryAssetResolverPath = pluginsRoot + "/InMemoryAssetResolver/resources"
 cesiumUsdSchemasPath = pluginsRoot + "/CesiumUsdSchemas/resources"
 
