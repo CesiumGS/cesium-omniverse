462d030a2ec303412c9f39977d3564eb73ad4a73
Adam N. Morris <adam@cesium.com>
2023-03-15 14:03:52 -0500

    Implemented refresh tileset button

--- exts/cesium.omniverse/cesium/omniverse/ui/attributes/tileset_attributes_widget.py
+++ exts/cesium.omniverse/cesium/omniverse/ui/attributes/tileset_attributes_widget.py
@@ -1,6 +1,7 @@
 import logging
 from omni.kit.property.usd.custom_layout_helper import CustomLayoutFrame, CustomLayoutGroup, CustomLayoutProperty
 from omni.kit.property.usd.usd_property_widget import SchemaPropertiesWidget
+import omni.ui as ui
 from ...bindings import ICesiumOmniverseInterface
 from ...bindings.CesiumUsdSchemas import TilesetAPI as CesiumTilesetAPI
 
@@ -16,10 +17,15 @@ class CesiumTilesetAttributesWidget(SchemaPropertiesWidget):
     def clean(self):
         super().clean()
 
+    def on_refresh_button_clicked(self):
+        tileset_path = self._payload[0]
+        self._cesium_omniverse_interface.reload_tileset(tileset_path.pathString)
+
     def _customize_props_layout(self, props):
         frame = CustomLayoutFrame(hide_extra=True)
 
         with frame:
+            ui.Button("Refresh Tileset", clicked_fn=self.on_refresh_button_clicked)
             with CustomLayoutGroup("Source"):
                 CustomLayoutProperty("cesium:sourceType")
                 CustomLayoutProperty("cesium:ionAssetId")
