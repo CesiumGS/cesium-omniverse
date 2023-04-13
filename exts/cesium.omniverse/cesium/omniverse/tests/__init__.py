d362a9fdf3a09fd5c6ad4bf905920a540d162568
Adam N. Morris <adam@cesium.com>
2023-04-13 14:41:53 -0400

    Got Python unit tests working with some sample tests

---
+++ exts/cesium.omniverse/cesium/omniverse/tests/__init__.py
@@ -0,0 +1,5 @@
+# For Python testing within Omniverse, it only looks in the `.tests` submodule in whatever is defined
+# as an extensions Python module. For organization purposes, we then import all of our tests from our other
+# testing submodules.
+from .extension_test import *  # noqa: F401 F403
+from ..ui.models.tests import *  # noqa: F401 F403
--- exts/cesium.omniverse/cesium/omniverse/ui/models/tests/__init__.py
+++ exts/cesium.omniverse/cesium/omniverse/ui/models/tests/__init__.py
@@ -1,1 +1,1 @@
-from . import *
+from .date_model_test import *
