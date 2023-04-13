d30ea3a99b13441bd26d5591e377bcc745eb5604
Adam N. Morris <adam@cesium.com>
2023-04-13 17:32:40 -0400

    Fixed formatting

--- exts/cesium.omniverse/cesium/omniverse/ui/__init__.py
+++ exts/cesium.omniverse/cesium/omniverse/ui/__init__.py
@@ -12,4 +12,4 @@ from .credits_window import CesiumOmniverseCreditsWindow  # noqa: F401
 from .credits_viewport_frame import CesiumCreditsViewportFrame  # noqa: F401
 from .credits_parser import CesiumCreditsParser  # noqa: F401
 from .search_field_widget import CesiumSearchFieldWidget  # noqa: F401
-from .models import *
+from .models import *  # noqa: F401 F403
--- exts/cesium.omniverse/cesium/omniverse/ui/models/tests/__init__.py
+++ exts/cesium.omniverse/cesium/omniverse/ui/models/tests/__init__.py
@@ -1,1 +1,1 @@
-from .date_model_test import *
+from .date_model_test import *  # noqa: F401 F403
