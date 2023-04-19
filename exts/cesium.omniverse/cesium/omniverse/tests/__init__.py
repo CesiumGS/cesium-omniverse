a450a40fae026fdc09601ee1435576a64af309eb
Adam N. Morris <adam@cesium.com>
2023-04-19 10:48:57 -0500

    Added tests for cesium.omniverse.models

--- exts/cesium.omniverse/cesium/omniverse/tests/__init__.py
+++ exts/cesium.omniverse/cesium/omniverse/tests/__init__.py
@@ -2,4 +2,5 @@
 # as an extensions Python module. For organization purposes, we then import all of our tests from our other
 # testing submodules.
 from .extension_test import *  # noqa: F401 F403
+from ..models.tests import *  # noqa: F401 F403
 from ..ui.models.tests import *  # noqa: F401 F403
