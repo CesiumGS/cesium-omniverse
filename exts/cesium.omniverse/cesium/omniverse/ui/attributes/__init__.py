de7b021c1bdedc98e24710eb4d5560738b921837
Adam N. Morris <adam@cesium.com>
2023-06-07 17:42:24 -0500

    Added UI elements for the georeference prim

--- exts/cesium.omniverse/cesium/omniverse/ui/attributes/__init__.py
+++ exts/cesium.omniverse/cesium/omniverse/ui/attributes/__init__.py
@@ -1,3 +1,4 @@
 from .data_attributes_widget import CesiumDataSchemaAttributesWidget  # noqa: F401
+from .georeference_attributes_widget import CesiumGeoreferenceSchemaAttributesWidget  # noqa :F401
 from .tileset_attributes_widget import CesiumTilesetAttributesWidget  # noqa: F401
 from .imagery_attributes_widget import CesiumImageryAttributesWidget  # noqa: F401
