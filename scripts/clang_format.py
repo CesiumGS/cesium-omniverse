5acf7fb6c6c73e1132f56b817e7008564d114eb6
elser <Matt.Elser@cesium.com>
2023-06-08 11:48:56 -0400

    add scripts dir to flake8 checks and fix flake8 violations

--- scripts/clang_format.py
+++ scripts/clang_format.py
@@ -10,56 +10,42 @@ from typing import List
 
 
 def clang_format_on_path(clang_format_binary: str, absolute_path: Path) -> str:
-    cmd = "{} -style=file {}".format(shlex.quote(clang_format_binary),
-                                     shlex.quote(str(absolute_path)))
+    cmd = "{} -style=file {}".format(shlex.quote(clang_format_binary), shlex.quote(str(absolute_path)))
     cmd = shlex.split(cmd)
     result = subprocess.check_output(cmd)
-    return result.decode('utf-8', 'replace')
+    return result.decode("utf-8", "replace")
 
 
 def clang_format_in_place(clang_format_binary: str, absolute_path: Path):
-    cmd = "{} -style=file -i {}".format(shlex.quote(
-        clang_format_binary), shlex.quote(str(absolute_path)))
+    cmd = "{} -style=file -i {}".format(shlex.quote(clang_format_binary), shlex.quote(str(absolute_path)))
     cmd = shlex.split(cmd)
     subprocess.check_output(cmd)
 
 
 def parse_args(av: List[str]):
-    parser = argparse.ArgumentParser(
-        description='Run / check clang-formatting.')
-    parser.add_argument('--clang-format-executable',
-                        help='Specific clang-format binary to use.',
-                        action='store',
-                        required=False
-                        )
-    parser.add_argument('--source-directories',
-                        help='Directories (relative to project root) to recursively scan for cpp files (e.g "src", "include"...)',
-                        nargs='+',
-                        required=True
-                        )
+    parser = argparse.ArgumentParser(description="Run / check clang-formatting.")
+    parser.add_argument(
+        "--clang-format-executable", help="Specific clang-format binary to use.", action="store", required=False
+    )
+    parser.add_argument(
+        "--source-directories",
+        help='Directories (relative to project root) to recursively scan for cpp files (e.g "src", "include"...)',
+        nargs="+",
+        required=True,
+    )
     run_type = parser.add_mutually_exclusive_group(required=True)
-    run_type.add_argument('--fix',
-                          help='Apply clang-format formatting to source in-place (destructive)',
-                          action='store_true'
-                          )
-    run_type.add_argument('--check',
-                          help='Check if source matches clang-format rules',
-                          action='store_true'
-                          )
+    run_type.add_argument(
+        "--fix", help="Apply clang-format formatting to source in-place (destructive)", action="store_true"
+    )
+    run_type.add_argument("--check", help="Check if source matches clang-format rules", action="store_true")
     scope_type = parser.add_mutually_exclusive_group(required=True)
-    scope_type.add_argument('--all',
-                            help='Process all valid source files.',
-                            action='store_true'
-                            )
-    scope_type.add_argument('--staged',
-                            help='Process only staged source files.',
-                            action='store_true'
-                            )
+    scope_type.add_argument("--all", help="Process all valid source files.", action="store_true")
+    scope_type.add_argument("--staged", help="Process only staged source files.", action="store_true")
     return parser.parse_args(av)
 
 
 def main(av: List[str]):
-    if not shutil.which('git'):
+    if not shutil.which("git"):
         raise RuntimeError("Could not find git in path")
 
     project_root_directory = utils.get_project_root()
@@ -70,7 +56,7 @@ def main(av: List[str]):
     if clang_format_binary:
         clang_format_binary = shutil.which(clang_format_binary)
     if not clang_format_binary:
-        clang_format_binary = shutil.which('clang-format')
+        clang_format_binary = shutil.which("clang-format")
     if not clang_format_binary:
         raise RuntimeError("Could not find clang-format in system path")
 
@@ -78,19 +64,21 @@ def main(av: List[str]):
     source_directories = args.source_directories
 
     # Generate list of source_files to check / fix.
-    source_files: List[utils.SourceFile] = utils.get_all_source_files(
-        source_directories, args.all)
+    source_files: List[utils.SourceFile] = utils.get_all_source_files(source_directories, args.all)
     failed_files: List[utils.FailedFile] = []
 
     # Fix or check formatting for each file
     for src in source_files:
         absolute_path = project_root_directory.joinpath(src.relative_path)
         if args.check:
-            old_text = absolute_path.read_text(
-                encoding='utf-8') if not src.staged else utils.get_staged_file_text(src.relative_path)
+            old_text = (
+                absolute_path.read_text(encoding="utf-8")
+                if not src.staged
+                else utils.get_staged_file_text(src.relative_path)
+            )
             new_text = clang_format_on_path(clang_format_binary, absolute_path)
             diff = utils.unidiff_output(old_text, new_text)
-            if diff != '':
+            if diff != "":
                 failed_files.append(utils.FailedFile(src.relative_path, diff))
         else:
             clang_format_in_place(clang_format_binary, absolute_path)
@@ -115,5 +103,5 @@ def main(av: List[str]):
     sys.exit(len(failed_files))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
