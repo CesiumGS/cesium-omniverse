33cfd1868a331dc61bc1d52c895a99859522ae27
Sean Lilley <lilleyse@gmail.com>
2023-06-20 12:49:56 -0400

    Rename apps

--- scripts/run_python_unit_tests.bat
+++ scripts/run_python_unit_tests.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal
 set KIT_DIR=%~dp0\..\extern\nvidia\_build\target-deps\kit-sdk
-call "%KIT_DIR%\kit.exe"  --enable omni.kit.test --/app/enableStdoutOutput=0 --/exts/omni.kit.test/testExts/0='cesium.omniverse' --ext-folder "%KIT_DIR%/extscore" --ext-folder "%KIT_DIR%/exts" --ext-folder "%KIT_DIR%/apps" --ext-folder "%~dp0/../exts" --/exts/omni.kit.test/testExtApp="%~dp0/../apps/cesium.omniverse.app.kit" --/exts/omni.kit.test/testExtOutputPath="%~dp0/../_testoutput" --portable-root "%KIT_DIR%/" --/telemetry/mode=test %*
+call "%KIT_DIR%\kit.exe"  --enable omni.kit.test --/app/enableStdoutOutput=0 --/exts/omni.kit.test/testExts/0='cesium.omniverse' --ext-folder "%KIT_DIR%/extscore" --ext-folder "%KIT_DIR%/exts" --ext-folder "%KIT_DIR%/apps" --ext-folder "%~dp0/../exts" --/exts/omni.kit.test/testExtApp="%~dp0/../apps/cesium.omniverse.dev.kit" --/exts/omni.kit.test/testExtOutputPath="%~dp0/../_testoutput" --portable-root "%KIT_DIR%/" --/telemetry/mode=test %*
--- scripts/run_python_unit_tests.sh
+++ scripts/run_python_unit_tests.sh
@@ -2,4 +2,4 @@
 set -e
 SCRIPT_DIR=$(dirname ${BASH_SOURCE})
 KIT_DIR="$SCRIPT_DIR/../extern/nvidia/_build/target-deps/kit-sdk"
-exec "$KIT_DIR/kit" --enable omni.kit.test --/app/enableStdoutOutput=0 --/exts/omni.kit.test/testExts/0='cesium.omniverse' --ext-folder "$KIT_DIR/extscore" --ext-folder "$KIT_DIR/exts" --ext-folder "$KIT_DIR/apps" --ext-folder "$SCRIPT_DIR/../exts" --/exts/omni.kit.test/testExtApp="$SCRIPT_DIR/../apps/cesium.omniverse.app.kit" --/exts/omni.kit.test/testExtOutputPath="$SCRIPT_DIR/../_testoutput" --portable-root "$KIT_DIR/" --/telemetry/mode=test "$@"
+exec "$KIT_DIR/kit" --enable omni.kit.test --/app/enableStdoutOutput=0 --/exts/omni.kit.test/testExts/0='cesium.omniverse' --ext-folder "$KIT_DIR/extscore" --ext-folder "$KIT_DIR/exts" --ext-folder "$KIT_DIR/apps" --ext-folder "$SCRIPT_DIR/../exts" --/exts/omni.kit.test/testExtApp="$SCRIPT_DIR/../apps/cesium.omniverse.dev.kit" --/exts/omni.kit.test/testExtOutputPath="$SCRIPT_DIR/../_testoutput" --portable-root "$KIT_DIR/" --/telemetry/mode=test "$@"
