9bac4475089c57e207881ec797f7a2a3e2f59997
Cory Barr <cory@cesium.com>
2023-07-24 11:03:23 -0400

    tie in on_update_frame

--- exts/cesium.powertools/cesium/powertools/proceduralGeometries/procedural_geometries_window.py
+++ exts/cesium.powertools/cesium/powertools/proceduralGeometries/procedural_geometries_window.py
@@ -1,6 +1,9 @@
+import carb.events
 import logging
 import omni.ui as ui
 from cesium.omniverse.extension import _cesium_omniverse_interface as coi
+from typing import List
+import omni.kit.app as app
 
 
 class ProceduralGeometryWindow(ui.Window):
@@ -10,7 +13,7 @@ class ProceduralGeometryWindow(ui.Window):
 
     def __init__(self, **kwargs):
         super().__init__(ProceduralGeometryWindow.WINDOW_NAME, **kwargs)
-
+        self._subscriptions: List[carb.events.ISubscription] = []
         self._logger = logging.getLogger(__name__)
 
         # Set the function that is called to build widgets when the window is visible
@@ -34,12 +37,13 @@ class ProceduralGeometryWindow(ui.Window):
         self._logger.info(f"return val is {return_val}")
 
     def _alter_prims(self):
+        self._logger.info("inside _alter_prims")
         return_val = self._cesium_omniverse_interface.alter_procedural_prims()
         self._logger.info(f"return val is {return_val}")
 
     def _animate_prims(self):
-        return_val = self._cesium_omniverse_interface.animate_procedural_prims(.5)
-        self._logger.info(f"return val is {return_val}")
+        self._logger.info("inside _animate_prims")
+        self._setup_update_subscription()
 
     def _build_fn(self):
         """Builds out the UI buttons and their handlers."""
@@ -58,4 +62,17 @@ class ProceduralGeometryWindow(ui.Window):
             ui.Button("Alter prims", height=20, clicked_fn=self._alter_prims)
 
             ui.Button("Animate prims",  height=20, clicked_fn=self._animate_prims)
+
+    def _setup_update_subscription(self):
+        update_stream = app.get_app().get_update_event_stream()
+        self._subscriptions.append(
+            update_stream.create_subscription_to_pop(
+                self._on_update_frame, name="cesium.omniverse.viewport.ON_UPDATE_FRAME"
+            )
+        )
+
+    def _on_update_frame(self, _):
+        self._logger.info("inside _on_update_frame in ProdGeoWindow")
+        self._cesium_omniverse_interface.animate_procedural_prims(.5)
+
 
