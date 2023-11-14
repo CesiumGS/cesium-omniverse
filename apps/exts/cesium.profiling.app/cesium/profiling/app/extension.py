05b2d4d15fae3253621fed0f69ec9348316c9cb7
Cory Barr <cory@cesium.com>
2023-11-14 06:54:27 -0800

    make line flake-compliant

--- apps/exts/cesium.profiling.app/cesium/profiling/app/extension.py
+++ apps/exts/cesium.profiling.app/cesium/profiling/app/extension.py
@@ -1,13 +1,13 @@
-import omni.ext
 import carb.settings
 import asyncio
 from cesium.omniverse.utils import perform_action_after_n_frames_async
-import os
 from cesium.omniverse.utils.cesium_interface import CesiumInterfaceManager
+import omni.ext
 import omni.usd
 import omni.kit
 import omni.kit.commands
 import logging
+import os
 
 
 class CesiumProfilingExtension(omni.ext.IExt):
@@ -20,8 +20,9 @@ class CesiumProfilingExtension(omni.ext.IExt):
     def on_startup(self, ext_id):
         settings = carb.settings.get_settings()
         if (settings.get("/app/runProfilingScenes")):
-            # wait several seconds for Omniverse to load before running the first test
-            asyncio.ensure_future(perform_action_after_n_frames_async(120, self._run_profiling_suite))
+            FRAMES_BEFORE_TESTING_BEGINS = 120
+            asyncio.ensure_future(perform_action_after_n_frames_async(FRAMES_BEFORE_TESTING_BEGINS,
+                                                                      self._run_profiling_suite))
 
     def _run_profiling_suite(self):
         asyncio.ensure_future(self._run_profiling_suite_async())
@@ -30,9 +31,9 @@ class CesiumProfilingExtension(omni.ext.IExt):
         with CesiumInterfaceManager() as interface:
             interface.shut_down_profiling()
 
-    def _start_profiler(self, file_base_name):
+    def _start_profiler(self, file_basename):
         with CesiumInterfaceManager() as interface:
-            interface.initialize_profiling(file_base_name)
+            interface.initialize_profiling(file_basename)
 
     def _get_profiling_files(self, directory_path):
         files = []
@@ -53,10 +54,10 @@ class CesiumProfilingExtension(omni.ext.IExt):
         profiling_usd_files = self._get_profiling_files(test_directory)
 
         for usd_file in profiling_usd_files:
-            file_base_name = os.path.splitext(os.path.basename(usd_file))[0]
+            file_basename = os.path.splitext(os.path.basename(usd_file))[0]
             stage = omni.usd.get_context().open_stage(usd_file)
             if stage:
-                self._start_profiler(file_base_name)
+                self._start_profiler(file_basename)
                 omni.kit.commands.execute('ToolbarPlayButtonClicked')
                 await asyncio.sleep(scene_test_duration)
                 omni.kit.commands.execute('ToolbarStopButtonClicked')
