9110447914ff4d7f6790a62377b3e2a8e646f770
Cory Barr <cory@cesium.com>
2023-11-27 12:01:26 -0800

    add trivial OmniIonImagery class

---
+++ src/core/include/cesium/omniverse/OmniIonImagery.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "cesium/omniverse/OmniIonImagery.h"
+#include <CesiumIonClient/Token.h>
+#include <pxr/usd/sdf/path.h>
+
+namespace cesium::omniverse {
+class OmniIonImagery : public OmniIonImagery {
+};
+} // namespace cesium::omniverse
